223e4b384311f333d57fbef8e086be8d7ae280b3
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-24 01:15:27 -0600

    Creando estructura de proyecto, creacion de rutas, componentes y servicios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /* Lazyload */
+  {
+    path: '',
+    loadChildren: () => import('./paises/paises.module').then(m => m.PaisesModule)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
