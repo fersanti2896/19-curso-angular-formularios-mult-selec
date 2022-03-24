f09d562c355f6be3010704fa2680e70d1de77f3e
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-24 01:44:08 -0600

    Añadiendo selector de regiones

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
   selector: 'app-selector-page',
@@ -12,9 +13,14 @@ export class SelectorPageComponent implements OnInit {
     region: [ '', [ Validators.required ] ]
   });
 
-  constructor( private fb: FormBuilder ) { }
+  /* Llenar selectores */
+  regiones: string[] = [];
+
+  constructor( private fb: FormBuilder, 
+               private paisesService: PaisesService ) { }
 
   ngOnInit(): void {
+    this.regiones = this.paisesService.regiones;
   }
 
   guardar() {
